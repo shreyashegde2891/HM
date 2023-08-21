0327a485d4cf2cae0994eead5f16c1fe8e50ca34
shreyas hegde <shreyas.hegde91@gmail.com>
2023-08-21 19:00:39 +0530

    reverting local connection name for DB

--- db_models/database.py
+++ db_models/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:qwerty@pruinhlth-nprd-dev-scxlyx-7250:asia-south1:sahi-dev/HealthManagement"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:qwerty@34.100.218.48/HealthManagement"
 print("This package called")
 Base = declarative_base()
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
