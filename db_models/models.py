f9b7a48508d91dacbd794138a6bec13778b297b7
shreyas hegde <shreyas.hegde91@gmail.com>
2023-08-23 17:21:43 +0530

    Food recommendation API

--- db_models/models.py
+++ db_models/models.py
@@ -2,7 +2,13 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.dialects import postgresql
 from .database import Base
 from pydantic import BaseModel
-
+import numpy as np
+import re
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
+from typing import List, Optional
 class Questions(Base):
    __tablename__ = 'hraquestions'
    
@@ -29,7 +35,34 @@ class responseList(BaseModel):
    age: int
    gender:str
    answers: list[quesResponse]
+class params(BaseModel):
+    n_neighbors:int=5
+    return_distance:bool=False
 
+class PredictionIn(BaseModel):
+    calorie_goal:float
+    ingredients:list[str]=[]
+    params:Optional[params]
+
+class Recipe(BaseModel):
+    Name:str
+    CookTime:str
+    PrepTime:str
+    TotalTime:str
+    RecipeIngredientParts:list[str]
+    Calories:float
+    FatContent:float
+    SaturatedFatContent:float
+    CholesterolContent:float
+    SodiumContent:float
+    CarbohydrateContent:float
+    FiberContent:float
+    SugarContent:float
+    ProteinContent:float
+    RecipeInstructions:list[str]
+
+class PredictionOut(BaseModel):
+    output: Optional[list[Recipe]] = None
 class calculateScore():
    lifestyleScore = 0
    clinicalScore = 0
@@ -258,3 +291,60 @@ class calculateScore():
          "-0.5kgPerWeek": (a.bmr*0.8),
          "-1kgPerWeek":(a.bmr*0.6)
       }
+
+def scaling(dataframe):
+    scaler=StandardScaler()
+    prep_data=scaler.fit_transform(dataframe.iloc[:,6:15].to_numpy())
+    return prep_data,scaler
+
+def nn_predictor(prep_data):
+    neigh = NearestNeighbors(metric='cosine',algorithm='brute')
+    neigh.fit(prep_data)
+    return neigh
+
+def build_pipeline(neigh,scaler,params):
+    transformer = FunctionTransformer(neigh.kneighbors,kw_args=params)
+    pipeline=Pipeline([('std_scaler',scaler),('NN',transformer)])
+    return pipeline
+
+def extract_data(dataframe,ingredients):
+    extracted_data=dataframe.copy()
+    extracted_data=extract_ingredient_filtered_data(extracted_data,ingredients)
+    return extracted_data
+    
+def extract_ingredient_filtered_data(dataframe,ingredients):
+    extracted_data=dataframe.copy()
+    regex_string=''.join(map(lambda x:f'(?=.*{x})',ingredients))
+    extracted_data=extracted_data[extracted_data['RecipeIngredientParts'].str.contains(regex_string,regex=True,flags=re.IGNORECASE)]
+    return extracted_data
+
+def apply_pipeline(pipeline,_input,extracted_data):
+    _input=np.array(_input).reshape(1,-1)
+    return extracted_data.iloc[pipeline.transform(_input)[0]]
+
+def recommend(dataframe,_input,ingredients=[],params={'n_neighbors':5,'return_distance':False}):
+        extracted_data=extract_data(dataframe,ingredients)
+        if extracted_data.shape[0]>=params['n_neighbors']:
+            prep_data,scaler=scaling(extracted_data)
+            neigh=nn_predictor(prep_data)
+            pipeline=build_pipeline(neigh,scaler,params)
+            return apply_pipeline(pipeline,_input,extracted_data)
+        else:
+            return None
+
+def extract_quoted_strings(s):
+    # Find all the strings inside double quotes
+    strings = re.findall(r'"([^"]*)"', s)
+    # Join the strings with 'and'
+    return strings
+
+def output_recommended_recipes(dataframe):
+    if dataframe is not None:
+        output=dataframe.copy()
+        output=output.to_dict("records")
+        for recipe in output:
+            recipe['RecipeIngredientParts']=extract_quoted_strings(recipe['RecipeIngredientParts'])
+            recipe['RecipeInstructions']=extract_quoted_strings(recipe['RecipeInstructions'])
+    else:
+        output=None
+    return output
--- main.py
+++ main.py
@@ -9,10 +9,15 @@ from fastapi.encoders import jsonable_encoder
 from db_models.models import quesResponse
 from db_models.models import responseList
 from db_models.models import calculateScore
-from typing import List
+from typing import List, Optional
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from db_models.models import recommend,output_recommended_recipes
+from db_models.models import params, PredictionIn, Recipe, PredictionOut
+from random import uniform as rnd
 
 Base.metadata.create_all(bind=engine)
+dataset=pd.read_csv('data/dataset.csv',compression='gzip')
 
 session = Session()
 app = FastAPI()
@@ -24,6 +29,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 @app.get("/hraQuestions/clinical")
 async def get_questions(response_model=list[Questions]):
         return (getResponses(session,"Clinical"))
@@ -51,3 +58,31 @@ async def get_body(items:responseList):
         a=calculateScore(items)
         return (calculateScore.returnJson(a))
         #return (items)
+
+@app.post("/recommend/breakfast")
+def update_item(prediction_input:PredictionIn):
+    recommended_nutrition = [prediction_input.calorie_goal * 0.3,rnd(10,30),rnd(0,4),rnd(0,30),rnd(0,400),rnd(40,75),rnd(4,10),rnd(0,10),rnd(30,100)]
+    recommendation_dataframe=recommend(dataset,recommended_nutrition,prediction_input.ingredients,prediction_input.params.dict())
+    output=output_recommended_recipes(recommendation_dataframe)
+    if output is None:
+        return {"output":None}
+    else:
+        return {"output":output}
+@app.post("/recommend/lunch")
+def update_item(prediction_input:PredictionIn):
+    recommended_nutrition = [prediction_input.calorie_goal * 0.5,rnd(10,30),rnd(0,4),rnd(0,30),rnd(0,400),rnd(40,75),rnd(4,10),rnd(0,10),rnd(30,100)]
+    recommendation_dataframe=recommend(dataset,recommended_nutrition,prediction_input.ingredients,prediction_input.params.dict())
+    output=output_recommended_recipes(recommendation_dataframe)
+    if output is None:
+        return {"output":None}
+    else:
+        return {"output":output}
+@app.post("/recommend/dinner")
+def update_item(prediction_input:PredictionIn):
+    recommended_nutrition = [prediction_input.calorie_goal * 0.2,rnd(10,30),rnd(0,4),rnd(0,30),rnd(0,400),rnd(40,75),rnd(4,10),rnd(0,10),rnd(30,100)]
+    recommendation_dataframe=recommend(dataset,recommended_nutrition,prediction_input.ingredients,prediction_input.params.dict())
+    output=output_recommended_recipes(recommendation_dataframe)
+    if output is None:
+        return {"output":None}
+    else:
+        return {"output":output}
